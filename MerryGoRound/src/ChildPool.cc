ab90b7e3b13c06fbfad93317038902d2416e90f7
MatteBiondi <m.biondi7@studenti.unipi.it>
2022-12-22 17:09:31 +0100

    Created the SimpleModule for the arrival of children

---
+++ MerryGoRound/src/ChildPool.cc
@@ -0,0 +1,50 @@
+/*
+ * Project team:        Biondi, Cristofani, Guidotti
+ * Academic Year:       2022/2023
+ * File Description:    This is the module-related class for the simple
+ *                      module "ChildPool". This is the file containing
+ *                      implementation for the functions.
+ */
+
+#include "ChildPool.h"
+
+Define_Module(ChildPool);
+
+void ChildPool::initialize()
+{
+    // Initialize private attribute of the class
+    _Q = par("Q");
+    _Delta = par("Delta");
+    _Lambda = par("Lambda");
+    _P = par("P");
+
+    // Generate the message for next children creation
+    _nextArrival = new cMessage(CREATE_CHILDREN);
+    _time = exponential(_Lambda);
+    scheduleAt(simTime()+_time, _nextArrival);
+}
+
+void ChildPool::handleMessage(cMessage *msg)
+{
+    // Check that the received message is the self message (obvious for the module)
+    if(msg->isSelfMessage()){
+        // Extract a random number of children to be generated
+        int n = geometric(_P);
+
+        // Create message to be sent to ChildQueue
+        _newChildren = new ChildArrivalMsg();
+
+        // Generate n children and add them to the message
+        for (unsigned int i = 0; i < n; ++i){
+            double quitTime = _Q + exponential(_Delta);
+            _newChildren->addChild(quitTime);
+        }
+
+        // Send the message
+        send(_newChildren, "toChildQueue");
+
+        // Generate the message for next children creation
+        _time = exponential(_Lambda);
+        scheduleAt(simTime()+_time, _nextArrival);
+    }
+}
---
+++ MerryGoRound/src/ChildPool.h
@@ -0,0 +1,36 @@
+/*
+ * Project team:        Biondi, Cristofani, Guidotti
+ * Academic Year:       2022/2023
+ * File Description:    This is the module-related class for the simple
+ *                      module "ChildPool". This is the header file
+ *                      containing declarations for functions and
+ *                      attributes. The inclusion of this file in ".cc"
+ *                      file for the implementation is needed
+ */
+
+#ifndef __MERRYGOROUND_CHILDPOOL_H_
+#define __MERRYGOROUND_CHILDPOOL_H_
+
+#include <omnetpp.h>
+#include "ChildArrivalMsg.h"
+
+using namespace omnetpp;
+
+class ChildPool : public cSimpleModule
+{
+private:
+    ChildArrivalMsg *_newChildren;
+    cMessage *_nextArrival;
+    double _time;
+    // Parameters
+    int _Q;
+    double _Delta;
+    double _Lambda;
+    double _P;
+
+protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+#endif
