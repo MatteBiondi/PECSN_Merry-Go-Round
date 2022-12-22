16ef220f64d4e01c6ab0848e85cf410c54cb5b54
MatteBiondi <m.biondi7@studenti.unipi.it>
2022-12-22 16:25:22 +0100

    Created file for macro definition

---
+++ MerryGoRound/src/utility.h
@@ -0,0 +1,8 @@
+// Different values of message priority
+#define HIGH_PRIORITY   0
+#define MEDIUM_PRIORITY 1
+#define LOW_PRIORITY    2
+
+// Name of exchanged messages
+#define CHILD_ARRIVAL       "ChildArrivalMsg"
+#define CREATE_CHILDREN     "CreateChildren"
