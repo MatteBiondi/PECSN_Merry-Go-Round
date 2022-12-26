528b92f4a2283c47a83d02e685aaf18fbf52f2af
federico-cristofani <f.cristofani@studenti.unipi.it>
2022-12-26 16:52:34 +0100

    run simulations script

---
+++ MerryGoRound/simulations/run_simulations.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CONFIGURATIONS=(
+    "Validation_deterministic_test" 
+    "Validation_consistency_test" 
+    "Validation_degeneracy_test_Q_and_Delta_0" 
+    "Validation_degeneracy_test_N_0" 
+    "Validation_degeneracy_test_Lambda_0" 
+    "Validation_degeneracy_test_T_0"
+    "Validation_continuity_test_N"
+    "Validation_continuity_test_Delta"
+    "Validation_continuity_test_Lambda"
+    "Validation_continuity_test_T"
+    "Validation_continuity_test_P"
+)
+
+for config in ${CONFIGURATIONS[@]}; do
+  ../src/MerryGoRound_dbg -u Cmdenv -c $config -n .:../src omnetpp.ini
+done
+
+echo "Simulation complete"
