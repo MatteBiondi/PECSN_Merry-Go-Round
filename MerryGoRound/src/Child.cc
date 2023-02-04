0e5124da403ca90ff3cbf917e3eb50c1295ffd8b
MatteBiondi <m.biondi7@studenti.unipi.it>
2023-02-04 08:52:23 +0100

    Improving comments

--- MerryGoRound/src/Child.cc
+++ MerryGoRound/src/Child.cc
@@ -7,38 +7,60 @@
 
 #include "Child.h"
 
-// Initialize the static member variable
+// Initialize the static member variable for id assignment
 int Child::_nextId = 1;
 
-// Default constructor
+/**
+ * Child's default constructor
+ */
 Child::Child(){}
 
-// Constructor implementation
+/**
+ * Child's constructor implementation
+ *
+ * @param _quitTime: represents the simtime at which the child will exit the queue
+ */
 Child::Child(SimTime _quitTime) :
     _id(_nextId++), _quitTime(_quitTime) {}
 
-// Copy constructor implementation
+/**
+ * Child's copy constructor implementation
+ *
+ * @param other: Child's instance reference, to be used for new instance construction
+ */
 Child::Child(const Child &other) :
     _id(other._id), _quitTime(other._quitTime){}
 
-// Copy assignment operator implementation
+/**
+ * Child's copy assignment operator implementation
+ *
+ * @param other: Child's instance reference, to be used for instance initialization
+ */
 Child& Child::operator=(const Child &other) {
     _id = other._id;
     _quitTime = other._quitTime;
     return *this;
 }
 
-// Equality operator
+/**
+ * Child's equality operator
+ *
+ * @param other: Child's instance reference, to be used for comparison
+ */
 bool Child::operator==(const Child &other) const{
     return _id == other._id && _quitTime == other._quitTime;
 }
 
-// Get the value of the 'id' attribute
+/**
+ * Get the value of the 'id' attribute
+ */
 int Child::getId() const{
     return _id;
 }
 
-// Get the value of the 'quitTime' attribute
+/*
+ * Get the value of the 'quitTime' attribute
+ */
 SimTime Child::getQuitTime() const{
     return _quitTime;
 }
--- MerryGoRound/src/ChildArrivalMsg.cc
+++ MerryGoRound/src/ChildArrivalMsg.cc
@@ -7,22 +7,37 @@
 
 #include "ChildArrivalMsg.h"
 
-// Constructor
+/**
+ * ChildArrivalMsg's constructor. The message priority is set to 0
+ *
+ * @param name: message name ('ChildArrivalMsg' as default value)
+ * @param kind: message kind (0 as default value)
+ */
 ChildArrivalMsg::ChildArrivalMsg(const char* name, short kind) : cMessage(name, kind){
     setSchedulingPriority(HIGH_PRIORITY);
 };
 
-// Create and add a child to the vector
+/**
+ * Create and add a Child to the vector
+ *
+ * @param quitTime: represents the simtime at which the child will exit the queue
+ */
 void ChildArrivalMsg::addChild(SimTime quitTime){
     _children.emplace_back(quitTime);
 }
 
-// Get the number of children in the vector
+/**
+ * Get the number of children in the vector
+ */
 int ChildArrivalMsg::getNumChildren() const{
     return _children.size();
 }
 
-// Get a child from the vector
+/**
+ * Get a child from the vector
+ *
+ * @param index: vector index to be accessed
+ */
 Child ChildArrivalMsg::getChild(int index) const{
     return _children[index];
 }
